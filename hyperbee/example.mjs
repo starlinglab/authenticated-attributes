3385131e7bb09343484d56973ac338c22709938c
makeworld <25111343+makew0rld@users.noreply.github.com>
2023-04-07 13:05:46 -0400

    BSON hyperbee demo (#11)

---
+++ hyperbee/example.mjs
@@ -0,0 +1,22 @@
+import Hypercore from 'hypercore'
+import Hyperbee from 'hyperbee'
+import b4a from 'b4a'
+import { BSON } from 'bson'
+
+const core = new Hypercore('./demo-storage')
+
+// core.key and core.discoveryKey will only be set after core.ready resolves
+await core.ready()
+console.log('hypercore key:', b4a.toString(core.key, 'hex'))
+
+const db = new Hyperbee(core, {
+    keyEncoding: 'utf-8',
+    valueEncoding: 'binary'
+})
+
+// Put BSON value
+await db.put('key', BSON.serialize({ "sig": 123, "timestamp": new Uint8Array([0x0, 0x1, 0x2, 0x3]) }))
+
+// Decode and print BSON value
+const data = await db.get('key')
+console.log(BSON.deserialize(data.value))
