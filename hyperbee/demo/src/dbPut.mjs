27bb124c631ea0d822fdffb336a437e89f1288d9
makeworld <25111343+makew0rld@users.noreply.github.com>
2023-04-28 11:42:43 -0400

    Add encryption and dbGet (#24)
    Co-authored-by: Kate Sills <katelynsills@gmail.com>

--- hyperbee/demo/src/dbPut.mjs
+++ hyperbee/demo/src/dbPut.mjs
@@ -1,4 +1,4 @@
-import { encode, decode } from "@ipld/dag-cbor";
+import { encode } from "@ipld/dag-cbor";
 import { CID } from "multiformats";
 import * as ed from "@noble/ed25519";
 
@@ -9,10 +9,18 @@ import { makeKey } from "./makeKey.mjs";
 
 // TODO: load from elsewhere.
 // DO NOT USE IN PRODUCTION
-const privKey = ed.utils.randomPrivateKey();
+const privKey = Buffer.from(
+  "l/bAXV2FQcmsE1zK9P7s6Lih+Traa6hpg9vLRht2wys=",
+  "base64"
+);
 
 const dbPut = async (db, id, attr, value, encryptionKey = false) => {
-  const rawAttestation = { CID: CID.parse(id), attribute: attr, value };
+  const rawAttestation = {
+    CID: CID.parse(id),
+    attribute: attr,
+    value,
+    encrypted: Boolean(encryptionKey),
+  };
   const signature = await signAttestation(privKey, rawAttestation);
   const signedAttestation = {
     ...rawAttestation,
@@ -23,9 +31,10 @@ const dbPut = async (db, id, attr, value, encryptionKey = false) => {
 
   if (encryptionKey) {
     attestation = {
-      CID: id,
+      CID: CID.parse(id),
       attribute: attr,
       value: encryptValue(value, encryptionKey),
+      encrypted: true,
     };
   } else {
     attestation = rawAttestation;
--- hyperbee/demo/src/encryptValue.mjs
+++ hyperbee/demo/src/encryptValue.mjs
@@ -1,3 +1,19 @@
-const encryptValue = (v, encryptionKey) => "xxxxx";
+import pkg from "tweetnacl";
+const { secretbox, randomBytes } = pkg;
+import { encode } from "@ipld/dag-cbor";
 
-export { encryptValue };
+const newKey = () => {
+  return randomBytes(secretbox.keyLength);
+};
+
+const encryptValue = (obj, encryptionKey) => {
+  // Adapted from https://github.com/dchest/tweetnacl-js/wiki/Examples#secretbox
+  const nonce = randomBytes(secretbox.nonceLength);
+  const box = secretbox(encode(obj), nonce, encryptionKey);
+  const fullMessage = new Uint8Array(nonce.length + box.length);
+  fullMessage.set(nonce);
+  fullMessage.set(box, nonce.length);
+  return fullMessage;
+};
+
+export { encryptValue, newKey };
