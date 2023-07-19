5bd608855c4ebee2506f669dbcc08b793cfbc8a4
makeworld <makeworld@protonmail.com>
2023-07-19 16:58:44 -0400

    Disable copy feature by hiding icon

--- uwazi/entity-page/src/lib/Attestation.svelte
+++ uwazi/entity-page/src/lib/Attestation.svelte
@@ -58,6 +58,7 @@
     showAltModal = true;
   }
 
+  // eslint-disable-next-line no-unused-vars
   function downloadClick() {
     if (data.attestation.encrypted) {
       return;
@@ -111,6 +112,9 @@
       />
       <title>{signer}</title>
     </svg>
+    <!--
+      For now disable this feature (by hiding the icon only), see #31 and #38
+
     <svg
       class:disabled={data.attestation.encrypted}
       on:click={downloadClick}
@@ -118,10 +122,11 @@
       class="icon"
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 512 512"
-      ><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
+      ><path
         d="M448 384H256c-35.3 0-64-28.7-64-64V64c0-35.3 28.7-64 64-64H396.1c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9V320c0 35.3-28.7 64-64 64zM64 128h96v48H64c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16H256c8.8 0 16-7.2 16-16V416h48v32c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V192c0-35.3 28.7-64 64-64z"
       /></svg
     >
+    -->
   </div>
 </div>
 
@@ -265,9 +270,13 @@
     display: inline-block;
     width: 100%;
   }
+  /*
+  Used by removed clone/copy/download feature
+
   .disabled {
     opacity: 0.3;
   }
+  */
 
   #attr-modal-content {
     word-break: break-all;
