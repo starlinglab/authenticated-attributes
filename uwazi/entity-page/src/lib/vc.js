af71006d8360b821f17b673a7e23aff1c73fff72
makeworld <makeworld@protonmail.com>
2023-07-26 16:40:07 -0400

    Add VC unit test

---
+++ uwazi/entity-page/src/lib/vc.js
@@ -0,0 +1,96 @@
+import { uint8ArrayToBase64 } from "./shared.js";
+
+function isCID(cid) {
+  // The proper way to know would be to import the CID library and use `instanceof`
+  // But that would bloat our deps, so instead I'm hacking around it by
+  // checking for the existence of a property other objects likely won't have
+  return (
+    typeof cid === "object" &&
+    "multihash" in cid &&
+    "/" in cid &&
+    "asCID" in cid &&
+    "toV0" in cid &&
+    typeof cid.toV0 === "function"
+  );
+}
+
+function isJsonCID(k, v) {
+  // CID object becomes something else when being prepared for JSON
+  // Example: {"/": "bafyreietqpflteqz6kj7lmdqz76kzkwdo65o4bhivxrmqvha7pdgixxos4"}
+
+  if (k !== "/") {
+    return false;
+  }
+  try {
+    const c = Multiformats.CID.parse(v);
+    return Boolean(c);
+  } catch {
+    return false;
+  }
+}
+
+export function vcExport(data) {
+  const vc = {
+    "@context": [
+      "https://www.w3.org/2018/credentials/v1",
+      "urn:authattr:vc-schema:1",
+    ],
+    id: `urn:cid:${data.signature.signedMsg}`,
+    type: ["VerifiableCredential", "AuthAttrCredential"],
+    issuer: `urn:authattr:pubkey:${uint8ArrayToBase64(data.signature.pubKey)}`,
+    issuanceDate: data.attestation.timestamp,
+    credentialSubject: {
+      id: `urn:cid:${data.attestation.CID}`,
+      attribute: data.attestation.attribute,
+      value: data.attestation.value,
+      encoding: "json",
+    },
+    proof: {
+      type: "authattr_ed25519_v1",
+      pubKey: uint8ArrayToBase64(data.signature.pubKey),
+      signature: uint8ArrayToBase64(data.signature.signature),
+    },
+  };
+
+  if (vc.credentialSubject.encrypted) {
+    // Encode value otherwise the default bytes->JSON conversion will be used
+    // which is terrible
+    vc.credentialSubject.value = uint8ArrayToBase64(data.attestation.value);
+    vc.credentialSubject.encoding = "base64_encrypted_dag-cbor";
+    return JSON.stringify(vc);
+  }
+
+  // See if different encoding of value is required. If there are any bytes
+  // in data.attestation.value, the whole thing needs to be DAG-CBOR encoded
+  // to maintain integrity and allow for proper decoding.
+
+  let vcStr;
+  try {
+    vcStr = JSON.stringify(vc, (k, v) => {
+      if (
+        // Directly bytes
+        k instanceof Uint8Array ||
+        // Or it's a CID, which should be encoded properly so the sig is valid
+        isCID(k) ||
+        // Check values too
+        v instanceof Uint8Array ||
+        isCID(v) ||
+        // Finally check if it's a converted CID
+        isJsonCID(k, v)
+      ) {
+        throw new Error("binary");
+      }
+      return v;
+    });
+  } catch {
+    // Error occured, meaning there is binary data that must be encoded
+    vc.credentialSubject.value = uint8ArrayToBase64(
+      IpldDagCbor.encode(data.attestation.value)
+    );
+    vc.credentialSubject.encoding = "base64_dag-cbor";
+    return JSON.stringify(vc);
+  }
+
+  // No error, no binary data
+  return vcStr;
+}
---
+++ uwazi/entity-page/test/vc.test.js
@@ -0,0 +1,140 @@
+/* eslint-disable no-undef */
+
+import { CID } from "multiformats/cid";
+import * as Block from "multiformats/block";
+import { sha256 } from "multiformats/hashes/sha2";
+import * as dagCBOR from "@ipld/dag-cbor";
+import { verifyAsync } from "@noble/ed25519";
+
+import { vcExport } from "../src/lib/vc.js";
+
+function base64ToUint8Array(base64) {
+  return Uint8Array.from(atob(base64), (c) => c.charCodeAt(0));
+}
+
+async function verifyVC(vc) {
+  // Validate signature:
+  //   1. Recreate "attestation" object
+  //   2. Encode as DAG-CBOR
+  //   3. Check against signature field
+  const attestation = {
+    CID: CID.parse(vc.credentialSubject.id.slice(8)),
+    value: vc.credentialSubject.value,
+    encrypted: false,
+    timestamp: vc.issuanceDate,
+  };
+
+  switch (vc.credentialSubject.encoding) {
+    case "json":
+      break;
+    case "base64_dag-cbor":
+      attestation.value = dagCBOR.decode(
+        base64ToUint8Array(vc.credentialSubject.value)
+      );
+      break;
+    case "base64_encrypted_dag-cbor":
+      attestation.value = base64ToUint8Array(vc.credentialSubject.value);
+      attestation.encrypted = true;
+      break;
+    default:
+      throw new Error("unknown VC encoding");
+  }
+
+  // Get signed message (CID of DAG-CBOR)
+  const block = await Block.encode({
+    value: attestation,
+    codec: dagCBOR,
+    hasher: sha256,
+  });
+  return verifyAsync(
+    base64ToUint8Array(vc.proof.signature),
+    block.cid,
+    base64ToUint8Array(vc.proof.pubKey)
+  );
+}
+
+test("verify sig", async () => {
+  // Check export
+  const vcStr = vcExport({
+    signature: {
+      pubKey: base64ToUint8Array(
+        "67meek60cPVutfMasym54S0UOcceEtWrJbcgSp6QPDc="
+      ),
+      signature: base64ToUint8Array(
+        "xlSI5d4DisnP4NDgeqq+UYIoTauUy8ZgdczqJg/krZLfRuNvhgUIRaVEblZ5gjZ7QU9deZ/xcPZKG59aKy/lDg=="
+      ),
+      // CID of "attestation" object
+      signedMsg: CID.parse(
+        "bafyreiadeqqwoqzxa6ckri3f6rhozd37p6crtbjo5ljemzt2sbf3uuyq7m"
+      ),
+    },
+    timestamp: {}, // ignored for VC
+    attestation: {
+      // CID of asset file, same CID as in the database key
+      CID: CID.parse(
+        "bafybeifgkpgb7yqgjnovszaio7tzetmdfmigylr24hg6a76wnjxcnhkx54"
+      ),
+      value: "application/wacz",
+      attribute: "mime",
+      encrypted: false,
+      timestamp: "2023-07-26T19:05:06.882Z",
+    },
+  });
+  expect(vcStr).toBe(
+    '{"@context":["https://www.w3.org/2018/credentials/v1","urn:authattr:vc-schema:1"],' +
+      '"id":"urn:cid:bafyreiadeqqwoqzxa6ckri3f6rhozd37p6crtbjo5ljemzt2sbf3uuyq7m",' +
+      '"type":["VerifiableCredential","AuthAttrCredential"],' +
+      '"issuer":"urn:authattr:pubkey:67meek60cPVutfMasym54S0UOcceEtWrJbcgSp6QPDc=",' +
+      '"issuanceDate":"2023-07-26T19:05:06.882Z",' +
+      '"credentialSubject":{"id":"urn:cid:bafybeifgkpgb7yqgjnovszaio7tzetmdfmigylr24hg6a76wnjxcnhkx54",' +
+      '"attribute":"mime","value":"application/wacz","encoding":"json"},' +
+      '"proof":{"type":"authattr_ed25519_v1","pubKey":"67meek60cPVutfMasym54S0UOcceEtWrJbcgSp6QPDc=",' +
+      '"signature":"xlSI5d4DisnP4NDgeqq+UYIoTauUy8ZgdczqJg/krZLfRuNvhgUIRaVEblZ5gjZ7QU9deZ/xcPZKG59aKy/lDg=="}}'
+  );
+  const vc = JSON.parse(vcStr);
+  expect(await verifyVC(vc)).toBe(true);
+});
+
+test("verify float change", async () => {
+  // Float64 value with only one bit toggled on
+  // Shortest representation is 5e-324
+  // Most accurate representation is 4.94065645841246544177e-324
+  // Explore more here: https://float.exposed/0x0000000000000001
+  const smallFloat = 4.94065645841246544177e-324;
+  const vcStr = vcExport({
+    signature: {
+      pubKey: new Uint8Array(32).fill(1),
+      signature: new Uint8Array(64).fill(1),
+      // CID of "attestation" object
+      signedMsg: CID.parse(
+        "bafyreietqpflteqz6kj7lmdqz76kzkwdo65o4bhivxrmqvha7pdgixxos4"
+      ),
+    },
+    timestamp: {},
+    attestation: {
+      // CID of asset file, same CID as in the database key
+      CID: CID.parse(
+        "bafkreif7gtpfl7dwi5nflge2rsfp6vq6q5kkwfm7uvxyyezxhsnde5ly3y"
+      ),
+      value: smallFloat,
+      attribute: "test",
+      encrypted: false,
+      timestamp: "2023-05-29T19:03:28.601Z",
+    },
+  });
+  expect(vcStr).toBe(
+    '{"@context":["https://www.w3.org/2018/credentials/v1","urn:authattr:vc-schema:1"],' +
+      '"id":"urn:cid:bafyreietqpflteqz6kj7lmdqz76kzkwdo65o4bhivxrmqvha7pdgixxos4",' +
+      '"type":["VerifiableCredential","AuthAttrCredential"],' +
+      '"issuer":"urn:authattr:pubkey:AQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQE=",' +
+      '"issuanceDate":"2023-05-29T19:03:28.601Z",' +
+      '"credentialSubject":{"id":"urn:cid:bafkreif7gtpfl7dwi5nflge2rsfp6vq6q5kkwfm7uvxyyezxhsnde5ly3y",' +
+      '"attribute":"test","value":5e-324,"encoding":"json"},' +
+      '"proof":{"type":"authattr_ed25519_v1","pubKey":"AQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQE=",' +
+      '"signature":"AQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQ=="}}'
+  );
+  const vc = JSON.parse(vcStr);
+  expect(dagCBOR.decode(dagCBOR.encode(vc.credentialSubject.value))).toBe(
+    smallFloat
+  );
+});
