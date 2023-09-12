1604e21fc95dbdac55d29e5d840a79ac61527525
makeworld <makeworld@protonmail.com>
2023-09-12 14:38:53 -0400

    Add IPFS link if public_link is set

--- uwazi/entity-page/src/App.svelte
+++ uwazi/entity-page/src/App.svelte
@@ -81,6 +81,18 @@
     return datas;
   };
 
+  const checkPublicLink = (datas) => {
+    for (let i = 0; i < datas.length; i++) {
+      if (
+        "public_link" in datas[i] &&
+        Boolean(datas[i].public_link.attestation.value) !== false
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+
   /**
    * Walks through output of loadData, amalgamating entries from all sources.
    *
@@ -221,6 +233,7 @@
       const datas = await loadData(sources, cid);
       dbEntries = getDbEntries(sources, datas);
       entityInfo = getEntityInfo(cid);
+      publicLink = checkPublicLink(datas);
     } catch (e) {
       errMsg = e.toString();
       console.error(e);
@@ -321,6 +334,8 @@
   let newAttestation; // <NewAttestation> svelte component
 
   let relatedProps; // props for Related component, set dynamically
+
+  let publicLink = false; // Any source has a public_link attestation
 
   /// Main function ///
 
@@ -655,7 +670,25 @@
             {:else}
               <p>No file information is available for this CID.</p>
             {/if}
-            <p><strong>CID</strong> <code>{fileCid}</code></p>
+            <p>
+              <strong>CID</strong> <code>{fileCid}</code>
+              {#if publicLink}
+                <a
+                  href="https://ipfs.hypha.coop/ipfs/{fileCid}"
+                  target="_blank"
+                >
+                  <svg
+                    style="color: var(--theme1);"
+                    class="icon"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    ><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
+                      d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"
+                    /></svg
+                  >
+                </a>
+              {/if}
+            </p>
           </div>
           <div class="bottom-buttons">
             {#if !noSources}
